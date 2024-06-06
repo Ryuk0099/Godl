24816d672fa938c329018a0079e71b9888fe7b24
Ryuk0099 <kentadota7@gmail.com>
2024-06-06 20:52:38 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 import subprocess
 import time
+import os
 
 def restart_bot():
     while True:
         try:
-            # Replace 'your_bot_script.py' with the path to your bot script
-            process = subprocess.Popen(['python3', 'm.py'])
-            process.wait()
-        except Exception as e:
+            # Get the current directory
+            current_directory = os.path.dirname(os.path.abspath(__file__))
+            # Replace 'your_bot_script.py' with the name of your bot script
+            subprocess.run(['python3', os.path.join(current_directory, 'm.py')], check=True)
+        except subprocess.CalledProcessError as e:
             print(f'Bot crashed with error: {e}. Restarting...')
-        time.sleep(5)  # Wait for 5 seconds before restarting
+            time.sleep(5)  # Wait for 5 seconds before restarting
 
-if name == "main":
+if __name__ == "__main__":
     restart_bot()
